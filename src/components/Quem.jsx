78afc98047cd50081278de1c61b2b0e2e9b65e03
Takater <46070051+Takater@users.noreply.github.com>
2022-08-23 19:36:13 +0000

    Tue, Aug 23, 2022, 7:36 PM +00:00

--- src/components/Quem.jsx
+++ src/components/Quem.jsx
@@ -4,47 +4,47 @@ function Quem () {
     return (
         <div id="quemsoueu">
             <div className="row">
-                    <div className="col-md-6">
-                        <div className="card">
-                            <div className="card-body">
-                                <div id="content">
-                                    <ul className="timeline">
-                                        <li className="event" data-date="2001 - 2004">
-                                            <h3>Quem é Geovanni Sampaio?</h3>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta tortor vel orci tincidunt suscipit.</p>
-                                        </li>
-                                        <li className="event" data-date="2006 - 2007">
-                                            <h3>Formação e Estudos</h3>
-                                            <p>Get ready for an exciting event, this will kick off in amazing fashion with MOP.</p>
-                                        </li>
-                                        <li className="event" data-date="2006 - 2007">
-                                            <h3>Competições</h3>
-                                            <p>Get ready for an exciting event, this will kick off in amazing fashion with MOP.</p>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="card">
-                            <div className="card-body">
-                                <div id="content">
-                                    <ul className="timeline">
-                                        <li className="event" data-date="2007 - 2012">
-                                            <h3>Áreas de Atuação</h3>
-                                            <p>This is where it all goes down. You will compete head to head with your friends and rivals. Get ready!</p>
-                                        </li>
-                                        <li className="event" data-date="2012 - Atual">
-                                            <h3>Missão, Visão e Valores</h3>
-                                            <p>See how is the victor and who are the losers. The big stage is where the winners bask.</p>
-                                        </li>
-                                    </ul>
-                                </div>
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <div id="content">
+                                <ul className="timeline">
+                                    <li className="event" data-date="2001 - 2004">
+                                        <h3>Quem é Geovanni Sampaio?</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta tortor vel orci tincidunt suscipit.</p>
+                                    </li>
+                                    <li className="event" data-date="2006 - 2007">
+                                        <h3>Formação e Estudos</h3>
+                                        <p>Get ready for an exciting event, this will kick off in amazing fashion with MOP.</p>
+                                    </li>
+                                    <li className="event" data-date="2006 - 2007">
+                                        <h3>Competições</h3>
+                                        <p>Get ready for an exciting event, this will kick off in amazing fashion with MOP.</p>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-body">
+                            <div id="content">
+                                <ul className="timeline">
+                                    <li className="event" data-date="2007 - 2012">
+                                        <h3>Áreas de Atuação</h3>
+                                        <p>This is where it all goes down. You will compete head to head with your friends and rivals. Get ready!</p>
+                                    </li>
+                                    <li className="event" data-date="2012 - Atual">
+                                        <h3>Missão, Visão e Valores</h3>
+                                        <p>See how is the victor and who are the losers. The big stage is where the winners bask.</p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
