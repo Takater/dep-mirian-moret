60fda29bbd96a38c9acc7aeab880edfc5621d601
Takater <46070051+Takater@users.noreply.github.com>
2022-08-23 18:43:05 +0000

    Tue, Aug 23, 2022, 6:43 PM +00:00

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ function Header () {
         <div id="header">
             <img src={header} alt="Miriam"/>
             <ul style={listStyle} className="list-group list-group-horizontal">
-                <li className="list-group-item">QUEM SOU EU?</li>
+                <li className="list-group-item mx-auto">QUEM SOU EU?</li>
                 <li className="list-group-item">QUAIS PROJETOS EU JÁ FIZ?</li>
                 <li className="list-group-item">QUAIS OS PROJETOS PARA MEU MANDATO?</li>
             </ul>
