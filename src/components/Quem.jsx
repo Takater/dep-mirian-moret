ccb25dc762715fb2bcf040d826673d0e8861cbc2
Takater <46070051+Takater@users.noreply.github.com>
2022-08-23 20:01:34 +0000

    Tue, Aug 23, 2022, 8:01 PM +00:00

--- src/components/Quem.jsx
+++ src/components/Quem.jsx
@@ -8,8 +8,19 @@ function Quem () {
                     <div className="main-timeline5">
                         <div className="timeline">
                             <div className="timeline-content">
+                                
                                 <h3 className="title">Quem sou eu?</h3>
-                                <p className="description">Me chamo Mirian Moret de Freitas Lucena</p>
+                                <p className="description">Me chamo Mirian Moret de Freitas Lucena, tenho 56 anos, sou natural do
+                                Rio de Janeiro. Casada há 27 anos com Rogélio Pinheiro Lucena (em memória), tenho 4 lindos filhos.
+                                Filha do Pastor Joil Dias de Freitas e Noemia Moret de Freitas.</p>
+                            </div>
+                        </div>
+                        <div className="timeline">
+                            <div className="timeline-content">
+                                <h3 className="title">Quem sou eu?</h3>
+                                <p className="description">Me chamo Mirian Moret de Freitas Lucena, tenho 56 anos, sou natural do
+                                Rio de Janeiro. Casada há 27 anos com Rogélio Pinheiro Lucena (em memória), tenho 4 lindos filhos.
+                                Filha do Pastor Joil Dias de Freitas e Noemia Moret de Freitas.</p>
                             </div>
                         </div>
                     </div>
