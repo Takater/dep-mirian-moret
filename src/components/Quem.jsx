748f6111c6abac1c2a117e7bd04309a86b64ea6e
Takater <46070051+Takater@users.noreply.github.com>
2022-08-23 20:41:15 +0000

    Tue, Aug 23, 2022, 8:41 PM +00:00

--- src/components/Quem.jsx
+++ src/components/Quem.jsx
@@ -25,7 +25,7 @@ function Quem () {
                             </div>
                         </div>
                         <div className="timeline">
-                            <div className="timeline-icon"><span class="year">2003</span></div>
+                            <div className="timeline-icon"><span class="year">1996</span></div>
                             <div className="timeline-content">
                                 <h3 className="title">Meu currículo</h3>
                                 <p className="description">
@@ -40,16 +40,21 @@ function Quem () {
                             </div>
                         </div>
                         <div className="timeline">
-                            <div className="timeline-icon"><span class="year">1996</span></div>
+                            <div className="timeline-icon"><span class="year">2004</span></div>
                             <div className="timeline-content">
                                 <h3 className="title">Meu currículo</h3>
                                 <p className="description">
                                     <ul style={{listStyleType: 'circle'}}>
-                                        <li>Em 1996, assumi a direção da creche do presídio feminino - SUPEN (RO)</li>
-                                        <li>Em 1997, assumi a direção do grupo de recursos humanos - SUPEN (RO)</li>
-                                        <li>Em 1998, assumi a direção do albergue feminino - SUPEN (RO)</li>
-                                        <li>Entre 1999 a 2003, fui diretora da casa da adolescente - FASER</li>
-                                        <li>Ainda em 2003, assumi a direção da casa do adolescente e internação provisória - FASER</li>
+                                        <li>Em 2004, assumi a presidência da empresa municipal de transportes urbanos de
+                                            Ji-Paraná - EMTU</li>
+                                        <li>Também em 2004, em conjunto, assumi a Secretaria Municipal de Ação Social -
+                                            SEMAS (Ji-Paraná)</li>
+                                        <li>De 2005 a 2007, gerenciei o atendimento a criança e ao adolescente - FASER</li>
+                                        <li>De 2008 a 2018, fui chefe do setor de visita da penitenciária Estadual Ênio
+                                            dos Santos Pinheiro (RO)
+                                        </li>
+                                        <li>Em 2018, assumi a gerência de Saúde da Secretaria do Estado da Justiça</li>
+                                        <li>De 2019 a 2022, lotada na penitenciária feminina de Porto Velho</li>
                                     </ul>
                                 </p>
                             </div>
