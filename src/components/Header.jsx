dd11e50884eddb5a49ad3a38a1ac8111a53b701a
Takater <46070051+Takater@users.noreply.github.com>
2022-08-23 18:10:57 +0000

    Tue, Aug 23, 2022, 6:10 PM +00:00

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ function Header () {
     return (
         <div id="header">
             <img src={header} alt="Miriam"/>
-            <ul id="link-list">
-                <li>QUEM SOU EU?</li>
-                <li>QUAIS PROJETOS EU JÁ FIZ?</li>
-                <li>QUAIS OS PROJETOS PARA MEU MANDATO?</li>
+            <ul id="link-list" className="list-group list-group-horizontal">
+                <li className="list-group-item">QUEM SOU EU?</li>
+                <li className="list-group-item">QUAIS PROJETOS EU JÁ FIZ?</li>
+                <li className="list-group-item">QUAIS OS PROJETOS PARA MEU MANDATO?</li>
             </ul>
         </div>
     )
