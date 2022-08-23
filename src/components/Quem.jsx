f9c2c594e17f65a5012c8722452261e45a95c964
Takater <46070051+Takater@users.noreply.github.com>
2022-08-23 20:02:48 +0000

    Tue, Aug 23, 2022, 8:02 PM +00:00

--- src/components/Quem.jsx
+++ src/components/Quem.jsx
@@ -8,7 +8,7 @@ function Quem () {
                     <div className="main-timeline5">
                         <div className="timeline">
                             <div className="timeline-content">
-                                
+                                <div className="timeline-icon"><span class="year">1966</span></div>
                                 <h3 className="title">Quem sou eu?</h3>
                                 <p className="description">Me chamo Mirian Moret de Freitas Lucena, tenho 56 anos, sou natural do
                                 Rio de Janeiro. Casada há 27 anos com Rogélio Pinheiro Lucena (em memória), tenho 4 lindos filhos.
