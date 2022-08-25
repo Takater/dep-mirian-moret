a1d80969ca4fad9802e0ae428f96636324d85808
Takater <46070051+Takater@users.noreply.github.com>
2022-08-25 19:06:30 +0000

    Thu, Aug 25, 2022, 7:06 PM +00:00

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import Quem from './components/Quem'
 import Projetos from './components/Projetos'
+import Propostas from './components/Propostas'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Quem />
       <Projetos />
+      <Propostas />
     </div>
   );
 }
---
+++ src/components/Propostas.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Propostas () {
+    return (
+        <div id="propostas">
+
+        </div>
+    )
+}
+
+export default Propostas;
