608cbeee5b77ff3bd7a29089cf2f8324a8179061
Takater <46070051+Takater@users.noreply.github.com>
2022-08-25 20:07:15 +0000

    Thu, Aug 25, 2022, 8:07 PM +00:00

--- src/components/Propostas.jsx
+++ src/components/Propostas.jsx
@@ -5,7 +5,8 @@ function Propostas () {
         <div id="propostas">
             <h3>
                 A principal responsabilidade de um(a) Dep. Estadual é representar o povo dentro da Assembléia Legislativa do Estado
-                que foi eleito. Isso inclui: 
+                que foi eleito. Isso inclui criar, apresentar, e monitorar projetos de lei. Assim como fiscalizar a atuação do Executivo Estadual
+                (Governador). Sendo assim, como Deputada Estadual, irei criar projetos voltados para as áreas de Saúde, Educação e Segurança Pública.
             </h3>
         </div>
     )
