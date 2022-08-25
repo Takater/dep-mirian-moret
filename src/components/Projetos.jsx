72ea817c90656dfdd52ad1a3994100ead86fe6c6
Takater <46070051+Takater@users.noreply.github.com>
2022-08-25 15:30:05 +0000

    Thu, Aug 25, 2022, 3:30 PM +00:00

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -27,18 +27,35 @@ function Projetos () {
             >
                 <div className="row">
                     { projetos.map((projeto, ind) => {
-                        return ind >= 2 && (
+                        return ind <= 2 && (
                             <div className="col-md-4" key={ind}>
                                 <div className="card">
                                     <div className="card-body">
-                                        <img src={ projeto.image }/>
+                                        <img src={ projeto.image } alt={"Imagem: " + projeto.title}/>
                                         <h4 className="card-title">{ projeto.title }</h4>
                                         <p className="card-text">{ projeto.description }</p>
                                     </div>
                                 </div>
                             </div>
                         )
-                    )}}
+                        }
+                    )}
+                </div>
+                <div className="row">
+                    { projetos.map((projeto, ind) => {
+                        return ind > 2 && (
+                            <div className="col-md-4" key={ind}>
+                                <div className="card">
+                                    <div className="card-body">
+                                        <img src={ projeto.image } alt={"Imagem: " + projeto.title}/>
+                                        <h4 className="card-title">{ projeto.title }</h4>
+                                        <p className="card-text">{ projeto.description }</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                        }
+                    )}
                 </div>
             </Carousel>
         </div>
