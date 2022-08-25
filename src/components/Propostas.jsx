b5615bc9eae5a5addd3766b33bf12fa61e451194
Takater <46070051+Takater@users.noreply.github.com>
2022-08-25 19:56:42 +0000

    Thu, Aug 25, 2022, 7:56 PM +00:00

--- src/components/Propostas.jsx
+++ src/components/Propostas.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Propostas () {
     return (
         <div id="propostas">
-
+            <h3>
+                São partes das responsabilidades de um(a) Dep. Estadual: apresentar projetos, f
+            </h3>
         </div>
     )
 }
