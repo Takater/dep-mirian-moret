c5d0f06c50435957362589be9432a4a330b9e532
Takater <46070051+Takater@users.noreply.github.com>
2022-08-25 14:23:35 +0000

    Thu, Aug 25, 2022, 2:23 PM +00:00

---
+++ src/components/Projetos.jsx
@@ -0,0 +1,13 @@
+import React from './react'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Carousel } from 'react-responsive-carousel'
+
+function Projetos () {
+    return (
+        <div id="projetos">
+
+        </div>
+    )
+}
+
+export default Projetos;
