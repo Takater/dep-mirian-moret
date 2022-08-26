7513fd4a15b9b15e947cd814a2f775590c9f7a35
Takater <46070051+Takater@users.noreply.github.com>
2022-08-26 19:50:57 +0000

    Fri, Aug 26, 2022, 7:50 PM +00:00

--- src/components/Contatos.jsx
+++ src/components/Contatos.jsx
@@ -16,12 +16,12 @@ export default function Contatos () {
                     </em>
                 </div>
                 <div className="col-md-4">
-                    <ul >
-                    <li><a className="social-media-link" href="https://instagram.com/miriammoretlucena">
+                    <ul className="list-group">
+                    <li className="list-group-item"><a className="social-media-link" href="https://instagram.com/miriammoretlucena">
                         <InstagramIcon />
                         <span>miriammoretlucena</span>
                     </a></li>
-                    <li><a className="social-media-link" href="mailto:assessoriamirianmoret@gmail.com">
+                    <li className="list-group-item"><a className="social-media-link" href="mailto:assessoriamirianmoret@gmail.com">
                         <EmailIcon />
                         <span>assessoriamirianmoret@gmail.com</span>
                     </a></li>
