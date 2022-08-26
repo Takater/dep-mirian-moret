8bc6d56d9d29b63b4d375d0244cd220b7e04154a
Takater <46070051+Takater@users.noreply.github.com>
2022-08-26 19:58:54 +0000

    Fri, Aug 26, 2022, 7:58 PM +00:00

--- src/components/Contatos.jsx
+++ src/components/Contatos.jsx
@@ -9,7 +9,7 @@ export default function Contatos () {
             <div className="row">
                 <div className="col-md-4">
                     <em>
-                        <h1>Miriam Moret</h1>
+                        <h1>Mirian Moret</h1>
                         <h5>47.579.027/0001-44</h5>
                         <h3><strong>77123</strong></h3>
                         <h3 style={{color: '#fe6101'}}><img src={logo} alt="Logo Solidariedade"/>SOLIDARIEDADE</h3>
@@ -17,13 +17,16 @@ export default function Contatos () {
                 </div>
                 <div className="col-md-4">
                     <ul className="list-unstyled">
-                    <li><a className="social-media-link" href="https://instagram.com/miriammoretlucena">
+                    <li><a className="social-media-link" href="https://instagram.com/mirianmoretlucena">
                         <InstagramIcon />
-                        <span>miriammoretlucena</span>
+                        <span>mirianmoretlucena</span>
                     </a></li>
                     <li><a className="social-media-link" href="mailto:assessoriamirianmoret@gmail.com">
                         <EmailIcon />
                         <span>assessoriamirianmoret@gmail.com</span>
+                    </a></li>
+                    <li><a className="social-media-link" href="https://api.whatsapp.com/send?phone=5569992391429">
+                        
                     </a></li>
                     </ul>
                 </div>
