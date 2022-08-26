c49fa5b032c7151130fa153c447d2bd8a9de296e
Takater <46070051+Takater@users.noreply.github.com>
2022-08-26 18:53:01 +0000

    Fri, Aug 26, 2022, 6:53 PM +00:00

--- src/components/Contatos.jsx
+++ src/components/Contatos.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from 'https://www.solidariedade.org.br/media/2021/05/solidariedade-favicon.png'
 
 export default function Contatos () {
     return (
@@ -8,7 +9,7 @@ export default function Contatos () {
                     <em>
                     <h1>Miriam Moret</h1>
                     <h3><strong>77123</strong></h3>
-                    <p>Partido: Solidariedade</p>
+                    <p><img src={logo}/>Partido: Solidariedade</p>
                     <h5>47.579.027/0001-44</h5>
 
                     </em>
