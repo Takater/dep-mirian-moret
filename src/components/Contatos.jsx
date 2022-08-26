eefddf15ea1d644ca4967f090d230e7328da05d4
Takater <46070051+Takater@users.noreply.github.com>
2022-08-26 20:09:31 +0000

    Fri, Aug 26, 2022, 8:09 PM +00:00

--- src/components/Contatos.jsx
+++ src/components/Contatos.jsx
@@ -34,7 +34,8 @@ export default function Contatos () {
                 </div>
                 <div className="col-md-4">
                     <h1 style={{color: '#3a0ebc', textShadow: '1px 1px black'}}>Junte-se a nós!</h1>
-                    <h5>Entre no nosso grupo de whatsapp</h5>
+                    <h5>Entre no nosso grupo de whatsapp através do link abaixo</h5>
+                    <a href="https://chat.whatsapp.com/GuXZdfR7EhP5FopIL7vHPy">Amigos De Mirian Moret</a>
                 </div>
             </div>
         </div>
